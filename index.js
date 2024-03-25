81a03148e8fdcf9d50c9463b1d1cb263ca4b932c
oei55 <chnithryut1995@gmail.com>
2024-03-25 21:24:29 +0700

    New update

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+app.use(express.static('frontend'))
+app.use((req,res,next) => {
+    console.log("Web is on http://localhost:1080/")
+    next();
+})
+
+app.get("/",(req,res) => {
+res.sendFile(path.join(__dirname,"frontend","index.html"))
+
+
+})
+
+app.get("/About",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","About.html"))
+})
+app.get("*",(req,res) => {
+    
+})
+
+app.listen(1080 || process.env.PORT)

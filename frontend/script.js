a67ea8813c4fe7714efd4b7099e105016dd7df8d
Peakk2011 <peakk3984@gmail.com>
2024-04-02 07:39:29 +0700

    Responsive 1.3.5.4

--- frontend/script.js
+++ frontend/script.js
@@ -1,50 +1,50 @@
 const body = document.querySelector("body"),
-nav = document.querySelector("nav"),
-modeToggle = document.querySelector(".dark-light"),
-searchToggle = document.querySelector(".searchToggle"),
-sidebarOpen = document.querySelector(".sidebarOpen"),
-siderbarClose = document.querySelector(".siderbarClose");
+  nav = document.querySelector("nav"),
+  modeToggle = document.querySelector(".dark-light"),
+  searchToggle = document.querySelector(".searchToggle"),
+  sidebarOpen = document.querySelector(".sidebarOpen"),
+  siderbarClose = document.querySelector(".siderbarClose");
 let getMode = localStorage.getItem("mode");
 if (getMode && getMode === "dark-mode") {
-body.classList.add("dark");
+  body.classList.add("dark");
 }
 // js code to toggle dark and light mode
 modeToggle.addEventListener("click", () => {
-modeToggle.classList.toggle("active");
-body.classList.toggle("dark");
-// js code to keep user selected mode even page refresh or file reopen
-if (!body.classList.contains("dark")) {
+  modeToggle.classList.toggle("active");
+  body.classList.toggle("dark");
+  // js code to keep user selected mode even page refresh or file reopen
+  if (!body.classList.contains("dark")) {
     localStorage.setItem("mode", "light-mode");
-} else {
+  } else {
     localStorage.setItem("mode", "dark-mode");
-}
+  }
 });
 // js code to toggle search box
 searchToggle.addEventListener("click", () => {
-searchToggle.classList.toggle("active");
+  searchToggle.classList.toggle("active");
 });
 
 
 //   js code to toggle sidebar
 sidebarOpen.addEventListener("click", () => {
-nav.classList.add("active");
+  nav.classList.add("active");
 });
 body.addEventListener("click", e => {
-let clickedElm = e.target;
-if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
+  let clickedElm = e.target;
+  if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
     nav.classList.remove("active");
-}
+  }
 });
 
 var $loader = document.querySelector('.loader')
 
-window.onload = function() {
+window.onload = function () {
   $loader.classList.remove('loader--active')
 };
 
 document.querySelector('.btn').addEventListener('click', function () {
   $loader.classList.add('loader--active')
-  
+
   window.setTimeout(function () {
     $loader.classList.remove('loader--active')
   }, 5550)
@@ -54,15 +54,15 @@ function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-      if (elementTop < windowHeight - elementVisible) {
-          reveals[i].classList.add("active");
-      } else {
-          reveals[i].classList.remove("active");
-      }
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
   }
 }
 
@@ -75,57 +75,73 @@ const $hoverables = document.querySelectorAll('.hoverable');
 // Listeners
 document.body.addEventListener('mousemove', onMouseMove);
 for (let i = 0; i < $hoverables.length; i++) {
-    if (window.CP.shouldStopExecution(0)) break;
-    $hoverables[i].addEventListener('mouseenter', onMouseHover);
-    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+  if (window.CP.shouldStopExecution(0)) break;
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
 }
 
 // Move the cursor
 window.CP.exitedLoop(0); function onMouseMove(e) {
-    TweenMax.to($bigBall, .4, {
-        x: e.pageX - 15,
-        y: e.pageY - 15
-    });
+  TweenMax.to($bigBall, .4, {
+    x: e.pageX - 15,
+    y: e.pageY - 15
+  });
 
-    TweenMax.to($smallBall, .1, {
-        x: e.pageX - 5,
-        y: e.pageY - 8
-    });
+  TweenMax.to($smallBall, .1, {
+    x: e.pageX - 5,
+    y: e.pageY - 8
+  });
 
 };
 
 // Hover an element
 function onMouseHover() {
-    TweenMax.to($bigBall, .3, {
-        scale: 4
-    });
+  TweenMax.to($bigBall, .3, {
+    scale: 4
+  });
 
 };
 function onMouseHoverOut() {
-    TweenMax.to($bigBall, .3, {
-        scale: 1
-    });
+  TweenMax.to($bigBall, .3, {
+    scale: 1
+  });
 
 };
 // When the user clicks on div, open the popup
 // pop up section when click on view more section on navbar
 
 function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
 
 // When the user clicks on div, open the popup
 // pop up section when click on view more section on navbar
 
 function ON2() {
-    document.getElementById("overlay-moreOptions").style.display = "block";
+  document.getElementById("overlay-moreOptions").style.display = "block";
+}
+
+function Off2() {
+  document.getElementById("overlay-moreOptions").style.display = "none";
+  // Make on click on off2()
+};
+
+var scrollEventHandler = function () {
+  window.scroll(0, window.pageYOffset)
+}
+
+window.addEventListener("scroll", scrollEventHandler, false);
+
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
   }
-  
-  function Off2() {
-    document.getElementById("overlay-moreOptions").style.display = "none";
-    // Make on click on off2()
-  }
+);

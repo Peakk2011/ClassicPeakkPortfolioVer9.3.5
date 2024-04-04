8ff24be64523ca8feace8dc5af8a28e1bcf8fe68
Peakk2011 <peakk3984@gmail.com>
2024-04-04 18:18:01 +0700

    Patch 1.7 Function Notification

--- frontend/script.js
+++ frontend/script.js
@@ -145,3 +145,9 @@ var docWidth = document.documentElement.offsetWidth;
     }
   }
 );
+
+function launch_toast() {
+  var x = document.getElementById("toast")
+  x.className = "show";
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 5000);
+}
